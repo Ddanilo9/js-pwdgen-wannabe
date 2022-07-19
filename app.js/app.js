45970d70f4b9d269f3f92cbd6d03091c36e3df8b
Ddanilo9 <interiordanilo3@gmail.com>
2022-07-19 14:51:41 +0200

    Esercizio finito senza bonus

---
+++ app.js/app.js
@@ -0,0 +1,23 @@
+//Chiediamo all'utente il suo nome
+
+let nomeUtente = prompt ('Come ti chiami?')
+console.log(nomeUtente)
+//Chiediamo all'utente i suoi anni
+
+let anniUtente = prompt ('Quanti anni hai?')
+//trasformiamo il numero da stringa a number
+
+anniUtente = parseInt(anniUtente)
+console.log(anniUtente)
+
+//Chiediamo all'utente il suo colore preferito
+let coloreUtente = prompt('Quale è il tuo colore preferito?')
+console.log(coloreUtente)
+
+//prendo nell'html l'elemento che voglio sovrascrivere tramite l'ID
+let password = document.getElementById(psw)
+
+//stampo il contenuto di quello che voglio esca scritto 
+console.dir(password)
+psw.innerHTML = nomeUtente + anniUtente + coloreUtente + '26'
+
